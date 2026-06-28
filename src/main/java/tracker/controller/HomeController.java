f7b8ee594eb50ed254b3290c52d6ece8b9588d91
hardik <sainihardik638@gmail.com>
2026-06-28 21:31:39 +0530

    Fix health endpoint mapping

--- src/main/java/tracker/controller/HomeController.java
+++ src/main/java/tracker/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping("/heatlh")
+    @GetMapping("/health")
     public String home() {
         return "Fitness Tracker Backend API is running!";
     }
